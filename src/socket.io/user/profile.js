86de268822f97006bffc8efadda9b5ebd954627b
Biplop Ghimire <bghimire@andrew.cmu.edu>
2025-09-15 20:15:16 +0000

    Project 1: Refactor (src/user/blocks.js) - fixed blocks.can method having too many arguments

--- src/socket.io/user/profile.js
+++ src/socket.io/user/profile.js
@@ -45,7 +45,7 @@ module.exports = function (SocketUser) {
 		if (action !== 'block' && action !== 'unblock') {
 			throw new Error('[[error:unknow-block-action]]');
 		}
-		await user.blocks.can(socket.uid, blockerUid, blockeeUid, action);
+		await user.blocks.can({callerUid: socket.uid, blockerUid, blockeeUid}, action);
 		if (data.action === 'block') {
 			await user.blocks.add(blockeeUid, blockerUid);
 		} else if (data.action === 'unblock') {
