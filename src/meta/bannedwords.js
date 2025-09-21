1bb2d215ae8ce728211f0e07e2ee396915248780
Edison <kdao@andrew.cmu.edu>
2025-09-21 21:04:55 +0000

    added comments for info layer

--- src/meta/bannedwords.js
+++ src/meta/bannedwords.js
@@ -18,10 +18,12 @@ BannedWords._words = [
 	'abuse',
 ];
 
+// Reloads the in-memory cache from the database
 async function reloadFromDb() {
 	cache = await db.getSetMembers('banned-words') || [];
 	loaded = true;
 }
+
 
 BannedWords.init = async function () {
 	// Migrate hardcoded words to database if database is empty (testing only)
@@ -35,6 +37,7 @@ BannedWords.init = async function () {
 	pubsub.on('meta:bannedwords:reload', reloadFromDb);
 };
 
+// Returns a shallow copy of the current in-memory
 BannedWords.getList = function () {
 	// Sync getter used by validators
 	return cache.slice();
@@ -44,6 +47,7 @@ BannedWords.load = async function () {
 	return await db.getSetMembers('banned-words');
 };
 
+// REturned the cached list, ensuring it is initialized once
 BannedWords.getAll = async function () {
 	if (!loaded) {
 		await reloadFromDb();
@@ -80,6 +84,7 @@ BannedWords.exists = async function (word) {
 	return await db.isSetMember('banned-words', normalizedWord);
 };
 
+// Update a banned word by removing the old and adding the new
 BannedWords.update = async function (oldWord, newWord) {
 	if (!oldWord || !newWord) {
 		throw new Error('[[error:invalid-word]]');
@@ -88,7 +93,7 @@ BannedWords.update = async function (oldWord, newWord) {
 	await BannedWords.add(newWord);
 };
 
-
+// Returns true if the given text contains any banned words
 BannedWords.containsBannedWords = async function (text) {
 	if (!text) return false;
 	const words = await BannedWords.getAll();
