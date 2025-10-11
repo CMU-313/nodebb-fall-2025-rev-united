212601ebe141e9c304b110e1d4f75ec15ab227a1
Edison <kdao@andrew.cmu.edu>
2025-10-11 02:49:44 +0000

    removed debuggong console log

--- src/topics/events.js
+++ src/topics/events.js
@@ -83,7 +83,6 @@ Events.init = async () => {
 
 
 async function translateEventArgs(...args) {
-	console.log('ADITI_AGNI');
 	const event = args[0];
 	const language = args[1];
 	const prefix = args[2];
