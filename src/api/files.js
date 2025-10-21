e564a66614aa9677159989c433d034682562d262
Biplop Ghimire <bghimire@andrew.cmu.edu>
2025-10-21 20:57:32 +0000

    just testing VM CI workflow, only added a comment

--- src/api/files.js
+++ src/api/files.js
@@ -6,6 +6,8 @@ const filesApi = module.exports;
 
 // path assertion and traversal guarding logic is in src/middleware/assert.js
 
+//testing VM CI
+
 filesApi.delete = async (_, { path }) => await fs.unlink(path);
 
 filesApi.createFolder = async (_, { path }) => await fs.mkdir(path);
