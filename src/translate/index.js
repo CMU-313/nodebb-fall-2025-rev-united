5fd672a26c35b62130a34c44e0b9c446e9b0aabb
Biplop Ghimire <bghimire@andrew.cmu.edu>
2025-11-11 16:31:40 +0000

    fixing eslint indent issue

--- src/translate/index.js
+++ src/translate/index.js
@@ -9,13 +9,13 @@ const translatorApi = module.exports;
 // };
 
 translatorApi.translate = async function (postData) {
-    const TRANSLATOR_API = 'http://host.docker.internal:5000';
-    try {
-        const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
-        const data = await response.json();
-        return [data.is_english, data.translated_content];
-    } catch (e) {
-        // If the API is down or any error occurs, return false and the original postData
-        return [false, postData];
-    }
+	const TRANSLATOR_API = 'http://host.docker.internal:5000';
+	try {
+		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+		const data = await response.json();
+		return [data.is_english, data.translated_content];
+	} catch (e) {
+		// If the API is down or any error occurs, return false and the original postData
+		return [false, postData];
+	}
 };
