4a0c1cc3ad35887a1d4298a3fd09be48e9d04a04
Biplop Ghimire <bghimire@andrew.cmu.edu>
2025-11-11 01:39:58 +0000

    added guardrail to make sure failing api doesnt break tests

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,9 +4,21 @@
 
 const translatorApi = module.exports;
 
+// translatorApi.translate = async function (postData) {
+//     const TRANSLATOR_API = 'http://host.docker.internal:5000';
+//     const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+//     const data = await response.json();
+//     return [data.is_english, data.translated_content];
+// };
+
 translatorApi.translate = async function (postData) {
     const TRANSLATOR_API = 'http://host.docker.internal:5000';
-    const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
-    const data = await response.json();
-    return [data.is_english, data.translated_content];
+    try {
+        const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+        const data = await response.json();
+        return [data.is_english, data.translated_content];
+    } catch (e) {
+        // If the API is down or any error occurs, return false and the original postData
+        return [false, postData];
+    }
 };
